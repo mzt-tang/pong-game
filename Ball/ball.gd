d47a437ad61cb37fd55d9e83bc7119938978e506
Michael Tang <mzt.tang@gmail.com>
2023-09-02 22:22:48 +1200

    removed useless code

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 
-var SPEED = 300.0
+const SPEED = 300.0
 
 func _ready():
 	randomize()
 	velocity.x = [-1, 1][randi() % 2] * SPEED
-#	velocity.y = [-0.8, 0.8][randi() % 2] * SPEED
+	velocity.y = [-0.8, 0.8][randi() % 2] * SPEED
 	
 
 func _physics_process(delta):
-#	velocity *= SPEED
 	var collision_object = move_and_collide(velocity*delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.get_normal())
